063e68c78ff4c9d443ce67939225d51c4326682c
Cristian Parra <cristianpv123@gmail.com>
2019-09-23 00:06:58 -0430

    Prototipo con Sidebar y modulo de Cantina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+/* para que funcione el jquery
+declare var JQuery:any;
+declare var $:any;
+*/
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tienda-mascotas';
+	public title: string;
+	public show_menu: boolean;
+
+	constructor(){
+		this.title = 'tienda-mascotas';
+		this.show_menu = true;
+	}
+
+	ngOnInit(){
+		/*$(document).ready(function(){
+			if($("body").height() < $(window).height()){
+
+				$("footer").css({"position":"absolute","bottom":"0px"});
+			}
+		});*/
+	}
+
+	hideMenu(){
+		if(this.show_menu == true){
+			this.show_menu = false;
+		}else{
+			this.show_menu = true;
+		}
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { routing, appRoutingProviders } from './app.routing';
+
+//Components
+import { HomeComponent } from './components/home.component';
+import { HospedajeComponent } from './components/hospedaje.component';
+import { CantinaComponent } from './components/cantina.component';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HospedajeComponent,
+    CantinaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [
+  	appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
